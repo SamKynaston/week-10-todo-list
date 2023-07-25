f73093aa443597ef6ca5b7a594a98bc1142ae3ee
Sam <samuelkynno@gmail.com>
2023-07-25 15:22:41 +0100

    model fix

--- backend/services/models/user.js
+++ backend/services/models/user.js
@@ -1,7 +1,7 @@
 const {DataTypes} = require("sequelize")
 const database = require("../database")
 
-const ToDo = database.define("todo", {
+const Model = database.define("todo", {
     username: {type:DataTypes.STRING, allowNull:false, unique:true},
     email: {type:DataTypes.STRING, allowNull:false, unique:true},
     password: {type:DataTypes.STRING, allowNull:false},
