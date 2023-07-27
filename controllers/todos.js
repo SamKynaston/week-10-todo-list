53b5b814c2b3bb93cd8d555ff9b0bf87e9386f90
Sam <samuelkynno@gmail.com>
2023-07-27 09:50:09 +0100

    added in user verification

--- controllers/todos.js
+++ controllers/todos.js
@@ -6,11 +6,15 @@ const ToDo = require("../services/models/todo")
 
 const addActiveToDo = async (req, res) => {
     try {
-        if (!req["authCheck"]) { return res.status(401).json({message:"user not authorized"}) }
+        const user = await User.findOne({
+            where: req["authCheck"]
+        })
+        if (!req["authCheck"] || !user) { return res.status(401).json({message:"user not authorized"}) }
 
+        req.body["UserId"] = user.id
         req.body["isActive"] = true
         const newActiveToDo = ToDo.create(req.body)
-        
+
         return res.status(201).json({successResponse:{message:"success", todo:newActiveToDo}})
     } catch (err) {
         return res.status(501).json({errorResponse: {message:err.Message, error:err}})
@@ -20,11 +24,12 @@ const addActiveToDo = async (req, res) => {
 const deleteActiveToDo = async (req, res) => {
     try {
         if (!req["authCheck"]) { return res.status(401).json({message:"user not authorized"}) }
-
+        
         const todo = await ToDo.findOne({
             where: req.body
         })
 
+        if (!todo.UserId != req["authCheck"]["id"]) { return res.status(401).json({message:"user not authorized"}) }
         if (!todo) { return res.status(404).json({message:"todo not found"}) }
 
         todo.destroy()
@@ -37,9 +42,14 @@ const deleteActiveToDo = async (req, res) => {
 
 const addDoneToDo = async (req, res) => {
     try {
-        if (!req["authCheck"]) { return res.status(401).json({message:"user not authorized"}) }
+        const user = await User.findOne({
+            where: req["authCheck"]
+        })
+        if (!req["authCheck"] || !user) { return res.status(401).json({message:"user not authorized"}) }
 
+        req.body["UserId"] = user.id
         const newDoneToDo = ToDo.create(req.body)
+        
         return res.status(201).json({successResponse:{message:"success", todo:newDoneToDo}})
     } catch (err) {
         return res.status(501).json({errorResponse: {message:err.Message, error:err}})
@@ -55,6 +65,7 @@ const deleteDoneToDo = async (req, res) => {
         })
 
         if (!todo) { return res.status(404).json({message:"todo not found"}) }
+        if (!todo.UserId != req["authCheck"]["id"]) { return res.status(401).json({message:"user not authorized"}) }
 
         todo.destroy()
 
