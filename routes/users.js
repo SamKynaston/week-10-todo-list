5b3eb270d1930f9d1c25e8a071054bbb68e03ee1
Aaron Payne <aaron_payne@hotmail.co.uk>
2023-07-27 14:22:49 +0100

    added code to users on controllers and routes

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,14 @@
-const {Router} = require("express")
-const router = Router()
+const {Router} = require("express");
+const {registerUser, login, getAllUsers, updateUser, deleteUser} = require("../controllers/users");
+const {hashPass, authenticatePassword, authenticateToken} = require("../middleware")
 
-module.exports = router
+const userRouter = Router()
+
+userRouter.post("/users/register", hashPass, registerUser);
+userRouter.post("/users/login", authenticatePassword, login);
+userRouter.get("/users/getAllUsers", authenticateToken, getAllUsers);
+userRouter.get("/users/authCheck");
+userRouter.put("/users/updateUser", authenticateToken, updateUser);
+userRouter.delete("/users/deleteUser", authenticateToken, deleteUser)
+
+module.exports = userRouter
